abaa4763a9bf690305f78abafbd21112c4578d55
Mohanraj Gopi <mohanraj.gopi@perficient.com>
2023-07-31 20:11:10 +0530

    initial commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,46 @@
+import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import "./topbar.css";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Social</span>
+      </div>
+      <div className="topbarcenter">
+        <div className="searchbar">
+          <SearchIcon />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">3</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home() {
+  return <div><div><Topbar /></div>home page</div>;
+}
